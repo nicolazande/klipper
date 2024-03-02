5bf5aa436842f1ed3c4f405aaa737d76468b174c
nicola <nicolazande@gmail.com>
2024-03-02 12:27:38 +0100

    remote debug

--- klippy/extras/verify_heater.py
+++ klippy/extras/verify_heater.py
@@ -40,6 +40,7 @@ class HeaterCheck:
         logging.info("Starting heater checks for %s", self.heater_name)
         reactor = self.printer.get_reactor()
         self.check_timer = reactor.register_timer(self.check_event, reactor.NOW)
+        logging.info("POLENTARUTTI")
     def handle_shutdown(self):
         if self.check_timer is not None:
             reactor = self.printer.get_reactor()
