3df2aaf78eeb75b6ac07336c25bb5b462adc0887
nicola <nicolazande@gmail.com>
2024-04-24 09:06:36 +0200

    rdb

--- klippy/chelper/ethercatqueue.c
+++ klippy/chelper/ethercatqueue.c
@@ -791,16 +791,24 @@ process_frame(struct ethercatqueue *sq)
                     /* clear error and update slave sequence */
                     move->command.code = COE_CMD_CLEAR_ERRORS;
                     slave->seq_num = status->next_id;
-                }
-                else
-                {
-                    /* disable command (discarded by drive) */
-                    move->command.code = COE_CMD_NO_OPERATION;
+                    /* common fields */
+                    move->error_mask = 0xFF;
+                    move->command.type = COE_SEGMENT_MODE_CMD;
                 }
 
-                /* common fields */
-                move->error_mask = 0xFF;
-                move->command.type = COE_SEGMENT_MODE_CMD;
+                /** 
+                 * NOTE: uncomment the following block in case a dummy command
+                 *       needs to be sent every cycle.
+                 */
+                // else
+                // {
+                //     /* disable command (discarded by drive) */
+                //     move->command.code = COE_CMD_NO_OPERATION;
+                // }
+
+                // /* common fields */
+                // move->error_mask = 0xFF;
+                // move->command.type = COE_SEGMENT_MODE_CMD;
 
                 /**
                  * Check receive buffer status and perform automatic transition
