5e06ee9f4939e46f4ae98b15191176280bbcc7cf
Nicola Zandegiacomo <nicolazande@gmail.com>
2024-05-04 17:03:24 +0200

    rdb

--- klippy/chelper/ethercatqueue.c
+++ klippy/chelper/ethercatqueue.c
@@ -331,12 +331,12 @@ build_and_send_command(struct ethercatqueue *sq, double eventtime)
             *move = *((struct coe_ip_move *)qm->msg);
 
             /* update step sequence number (avoid overflow) */
-            uint8_t nseq = slave->seq_num & SEQ_NUM_MASK;
-            move->header.seq_num = nseq; //step sequence number
+            move->header.seq_num = slave->seq_num & SEQ_NUM_MASK;; //step sequence number
 
+            uint8_t nseq = slave->seq_num % 32;
             slave->time_track[nseq] = clock_to_time(&sq->ce, qm->req_clock);
             double tmp_min_time = clock_to_time(&sq->ce, qm->min_clock);
-            //errorf("time track: (oid = %u, seq = %u, req_time = %lf, min_time = %lf, event_time = %lf)", slave->oid, nseq, slave->time_track[nseq], tmp_min_time, eventtime);
+            errorf("time track: (oid = %u, seq = %u, req_time = %lf, min_time = %lf, event_time = %lf)", slave->oid, nseq, slave->time_track[nseq], tmp_min_time, eventtime);
 
             /* update step sequence number (avoid overflow) */
             slave->seq_num++;
