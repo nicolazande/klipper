712edbe6d056df2db7519debedaeea14f2bea8e1
Nicola Zandegiacomo <nicolazande@gmail.com>
2024-05-05 14:44:03 +0200

    rdb

--- klippy/chelper/ethercatqueue.c
+++ klippy/chelper/ethercatqueue.c
@@ -338,7 +338,8 @@ build_and_send_command(struct ethercatqueue *sq, double eventtime)
             slave->time_table[nseq] = clock_to_time(&sq->ce, qm->req_clock);
 
             /* get next queue message */
-            struct move_segment_msg * nqm = list_next_entry(qm, node);
+            struct move_segment_msg *nqm = NULL;
+            while ((nqm = list_next_entry(qm, node)) != NULL && nqm->oid != qm->oid);
             if (nqm)
             {
                 double next_time = clock_to_time(&sq->ce, nqm->req_clock);
