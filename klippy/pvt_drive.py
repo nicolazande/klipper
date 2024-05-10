dd56e16683c4e73fe62f7943e061d76be3a7b622
Nicola Zandegiacomo <nicolazande@gmail.com>
2024-05-10 11:02:16 +0200

    rdb

--- klippy/pvt_drive.py
+++ klippy/pvt_drive.py
@@ -328,7 +328,6 @@ class PVT_drive:
         ffi_main, ffi_lib = chelper.get_ffi()
         # update last drive position
         last_pos = ffi_lib.pvtcompress_set_last_position(self._stepqueue, clock, last_pos)
-        self.set_position(last_pos*[4])
         sk = self._stepper_kinematics
         ffi_lib.pvtsolve_set_position(sk, last_pos, last_pos, last_pos)
         self._set_mcu_position(last_pos)
